28591be9df7e5217d3f3e680945197224e586cfe
Thiago009 <thiago.kaique01@gmail.com>
2024-08-06 00:06:49 -0300

    added a new function

---
+++ GLOBAL Go to offset.py
@@ -0,0 +1,23 @@
+#@author 
+#@category _NEW_
+#@keybinding ctrl alt b
+#@menupath 
+#@toolbar 
+
+from ghidra.app.script import GhidraScript
+from ghidra.util.task import ConsoleTaskMonitor
+from  _common import AddressLibrary
+
+
+scriptName = "Bookmark address library"
+
+
+scriptName = "Bookmark address library"
+class MyScript(GhidraScript):
+	def run(self):
+		offset = askInt(scriptName, "Please enter the offset")
+		func = getFunctionContaining(currentAddress)
+		goTo(func.getEntryPoint().add(offset))
+
+script = MyScript()
+script.run()
